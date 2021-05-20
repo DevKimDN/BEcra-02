ac706fa172256db14d851c3551c66c56406062ad
DevKimDN <kimtrong.vhit@gmail.com>
2021-05-20 07:19:09 +0700

    int01

---
+++ server.js
@@ -0,0 +1,29 @@
+//dotenv
+require('dotenv').config();
+
+const express = require ('express')
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); //Body parser
+
+
+
+const port = process.env.APP_PORT;
+
+
+app.get("/",(req,res,next) =>{
+res.status(200).json({
+    status: 'success',
+    data: {
+        num: 9999
+    }
+})   
+})
+
+
+app.listen(port, ()=> {
+    console.log(`server is running !!! on port ${port}`)
+})
